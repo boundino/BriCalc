0c4a2a908a87839c3979d25de46e79bca21c9368
Jing Wang <boundino@gmail.com>
2016-11-08 19:24:57 +0100

    first commit

---
+++ brilCalc.sh
@@ -0,0 +1,20 @@
+#  GOLDEN JSON
+# /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/5TeV/Cert_262081-262328_5TeV_PromptReco_Collisions15_25ns_JSON.txt
+# /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_v2.txt
+#  DIMUON JSON
+# /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/5TeV/Cert_262081-262328_5TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt
+# /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_MuonPhys_v2.txt
+#  TRACKERONLY JSON
+# /afs/cern.ch/work/j/jisun/public/forjing/Cert_263685-263757_PromptReco_HICollisions15_TrackerOnly_JSON.txt
+
+
+brilcalc lumi -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_v2.txt
+echo
+
+
+# HLT_HIL1MinimumBiasHF2AND_part1_v1
+# HLT_HIDmesonHITrackingGlobal_Dpt20_v1
+
+
+
+
